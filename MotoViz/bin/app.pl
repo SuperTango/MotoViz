7992b22e5cec1880a02ea056aca66280b30b853a
Alex.Tang <altitude@proofpoint.com>
2011-09-05 20:28:26 -0700

    Attempted to try to make Apache multiple servers work via the apache psgi Plack handler.  Didn't work well

--- MotoViz/bin/app.pl
+++ MotoViz/bin/app.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 use Dancer;
 use UIRoutes;
+#Dancer::App->set_running_app ( 'UI_MotoViz' );
 $ENV{'TMPDIR'} = '/funk/home/altitude/MotoViz/MotoViz/var/tmp';
 dance;
--- MotoViz_API/bin/app.pl
+++ MotoViz_API/bin/app.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 use Dancer;
 use RESTRoutes;
+#Dancer::App->set_running_app ( 'API_MotoViz' );
 $ENV{'TMPDIR'} = '/funk/home/altitude/MotoViz/MotoViz_API/var/tmp';
 dance;
