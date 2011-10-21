fa005b65760d6fe6c4d37e4f7157a7f37f42f120
Alex.Tang <altitude@funwkare.com>
2011-10-20 21:03:49 -0700

    cleanup of unneeded code

--- MotoViz_API/lib/RESTRoutes.pm
+++ MotoViz_API/lib/RESTRoutes.pm
@@ -44,30 +44,6 @@ get '/v1/test' => sub {
     return_json { message => 'Good!' };
 };
 
-get '/v1/points/:user_id/:ride_id' => sub {
-    my $ride_info = MotoViz::RideInfo::getRideInfo ( params->{'user_id'}, params->{'ride_id'} );
-    if ( ! $ride_info ) {
-        status 'not_found';
-        return;
-    }
-    my $params = params;
-    my $limit_points = params->{'limit_points'};
-    my @metrics = $params->{'metrics'} || ( 'battery_volts', 'battery_amps', 'speed_gps', 'speed_sensor', 'bearing', 'lat', 'lon', 'altitude', 'watts', 'wh', 'whPerMile', 'milesPerKWh', 'distance_gps_delta'   );
-    my $ret = fetch_points_average ( $ride_info, $limit_points, \@metrics );
-#    my $points = fetch_points_instant ( $ride_info, $limit_points );
-#     my $ret = {};
-#     foreach my $point ( @{$points} ) {
-#         foreach my $metric ( @metrics ) {
-#             my $time = $point->{'time'} * 1000;
-#             if ( ! exists $ret->{$metric} ) {
-#                 $ret->{$metric} = [];
-#             }
-#             push ( @{$ret->{$metric}}, [ $time, ( $point->{$metric} ) ? $point->{$metric} + 0 : 0 ] );
-#         }
-#     }
-    return return_json ( $ret );
-};
-
 del '/v1/ride/:user_id/:ride_id' => sub {
     my $ride_path = setting ( 'raw_log_dir' ) . '/' . params->{'user_id'} . '/' . params->{'ride_id'};
     if ( ! -d $ride_path ) {
@@ -255,12 +231,6 @@ sub process_ride {
     }
 };
 
-# get '/v1/reprocess/:user_id/:ride_id' => sub {
-#     my $ride_info = MotoViz::RideInfo::getRideInfo ( params->{'user_id'}, params->{'ride_id'} );
-#     my $ret = process_input_files ( $user_id, $ride_id, $title, $visibility );
-#     return_json $ret, { pretty => 1 };
-# };
-
 sub process_input_files {
     my $user_id = shift;
     my $ride_id = shift;
@@ -270,122 +240,7 @@ sub process_input_files {
     my $ride_path = setting ( 'raw_log_dir' ) . '/' . $user_id . '/' . $ride_id;
     my $output_processor = new MotoViz::OutputProcessor();
     $output_processor->init ( $user_id, $ride_id, $title, $visibility, $input_processor );
-    return $output_processor->generateOutputFile ( $ride_path . '/motoviz_output.out' );
+    return $output_processor->generateOutputFiles ( $ride_path );
 };
-
-
-sub fetch_points_average {
-    my $ride_info = shift;
-    my $limit_points = shift;
-    my $metrics = shift;
-    
-    my $points_count = $ride_info->{'points_count'};
-    my $fh;
-    my $ride_file = setting ( 'raw_log_dir' ) . '/' . $ride_info->{'user_id'} . '/' . $ride_info->{'ride_id'} . '/motoviz_output.out';
-    my $points = [];
-    my $should_fetch;
-
-        #
-        # $mod is the ( total number of points / ( limit_points - 1 ) ).
-        # to figure out which points from the original set to put in the final set,
-        # keep track of the last int ( $point_num / $mod ).  If the int value 
-        # is different than the last one, include the point.  Otherwise,
-        # discard the point.
-        #
-    my $mod = ( $limit_points ) ? $points_count / ( $limit_points ) : 0;
-    my $lastInt = -1;
-    my $count = 0;
-    my $avgCount = 0;
-
-    open ( $fh, $ride_file ) || die;
-    my $data = {};
-    my $sums = {};
-    foreach my $metric ( @{$metrics} ) {
-        $sums->{$metric} = 0;
-        $data->{$metric} = [];
-    }
-    while ( my $line = <$fh> ) {
-        $should_fetch = 0;
-        $avgCount++;
-        if ( $limit_points ) {
-            my $tmp = int ( $count / $mod );
-            if ( $tmp != $lastInt ) {
-                $lastInt = $tmp;
-                $should_fetch = 1;
-            }
-        } else {
-            $should_fetch = 1;
-        }
-
-        my $raw_point = from_json ( $line );
-
-        foreach my $metric ( @{$metrics} ) {
-            $sums->{$metric} += ( $raw_point->{$metric} ) ? $raw_point->{$metric} : 0;
-        }
-
-        my $time = $raw_point->{'time'} * 1000;
-        push ( @{$data->{'lat'}}, [ $time, ( $raw_point->{'lat'} ) ? $raw_point->{'lat'} + 0 : 0 ] );
-        push ( @{$data->{'lon'}}, [ $time, ( $raw_point->{'lon'} ) ? $raw_point->{'lon'} + 0 : 0 ] );
-        if ( $should_fetch ) {
-            #debug ( "avgCount: " . $avgCount );
-            foreach my $metric ( @{$metrics} ) {
-                if ( ( $metric ne 'lat' ) && ( $metric ne 'lon' ) ) {
-                    my $avg = $sums->{$metric} / $avgCount;
-                    #debug ( $metric . ', ' . $sums->{$metric} . ', avgCount: ' . $avgCount . ', avg: ' . $avg );
-                    push ( @{$data->{$metric}}, [ $time, ( $avg ) ? $avg + 0 : 0 ] );
-                }
-            }
-            foreach my $metric ( @{$metrics} ) {
-                $sums->{$metric} = 0;
-            }
-            $avgCount = 0;
-        }
-        $count++;
-    }
-    return $data;
-}
-
-sub fetch_points_instant {
-    my $ride_info = shift;
-    my $limit_points = shift;
-    
-    my $points_count = $ride_info->{'points_count'};
-    my $fh;
-    my $ride_file = setting ( 'raw_log_dir' ) . '/' . $ride_info->{'user_id'} . '/' . $ride_info->{'ride_id'} . '/motoviz_output.out';
-    my $points = [];
-    my $should_fetch;
-
-        #
-        # $mod is the ( total number of points / ( limit_points - 1 ) ).
-        # to figure out which points from the original set to put in the final set,
-        # keep track of the last int ( $point_num / $mod ).  If the int value 
-        # is different than the last one, include the point.  Otherwise,
-        # discard the point.
-        #
-    my $mod = ( $limit_points ) ? $points_count / ( $limit_points ) : 0;
-    my $lastInt = -1;
-    my $count = 0;
-
-    open ( $fh, $ride_file ) || die;
-    while ( my $line = <$fh> ) {
-        $should_fetch = 0;
-        if ( $limit_points ) {
-            my $tmp = int ( $count / $mod );
-            if ( $tmp != $lastInt ) {
-                $lastInt = $tmp;
-                $should_fetch = 1;
-            }
-        } else {
-            $should_fetch = 1;
-        }
-
-        if ( $should_fetch ) {
-            my $raw_point = from_json ( $line );
-            push ( @{$points}, $raw_point );
-        }
-        $count++;
-    }
-    return $points;
-}
 
 true;
